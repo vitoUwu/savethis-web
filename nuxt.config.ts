c928b0f8d0ed189f69b4852ce77be9f62fc9243b
vitoUwu <victorhugomeurerdelgrandi@gmail.com>
2024-09-03 19:48:50 -0400

    feat: add analytics

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,9 +2,16 @@
 export default defineNuxtConfig({
   compatibilityDate: "2024-04-03",
   devtools: { enabled: true },
-  modules: ["@nuxtjs/tailwindcss", "nuxt-mongoose"],
+  modules: [
+    "@nuxtjs/tailwindcss",
+    "nuxt-mongoose",
+    "@zadigetvoltaire/nuxt-gtm",
+  ],
   tailwindcss: {
     cssPath: "~/assets/css/tailwind.css",
     configPath: "./tailwind.config.ts",
+  },
+  gtm: {
+    id: "GTM-WP8DL534",
   },
 });
